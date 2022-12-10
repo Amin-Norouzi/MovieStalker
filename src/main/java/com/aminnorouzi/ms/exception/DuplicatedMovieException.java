5e52a55e05529723e21a8af6a747a58810f19823
aminnorouzi <aminnorouzidev@gmail.com>
2022-12-10 03:36:18 +0330

    Implemented DuplicatedMovieException class

---
+++ src/main/java/com/aminnorouzi/ms/exception/DuplicatedMovieException.java
@@ -0,0 +1,9 @@
+package com.aminnorouzi.ms.exception;
+
+public class DuplicatedMovieException extends RuntimeException {
+
+    public DuplicatedMovieException(String message) {
+        super(message);
+    }
+
+}
