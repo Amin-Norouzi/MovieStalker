1cb94e018ab8ce8258361dc82387ae4bbb86fa40
aminnorouzi <aminnorouzidev@gmail.com>
2022-12-10 03:38:00 +0330

    Removed existsByUsername method

--- src/main/java/com/aminnorouzi/ms/repository/UserRepository.java
+++ src/main/java/com/aminnorouzi/ms/repository/UserRepository.java
@@ -9,7 +9,5 @@ import java.util.Optional;
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
 
-    boolean existsByUsername(String username);
-
     Optional<User> findByUsername(String username);
 }
