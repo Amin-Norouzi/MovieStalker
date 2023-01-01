676d5b2903abf7b4fc249e48ae99249e5e5a19f8
aminnorouzi <aminnorouzidev@gmail.com>
2023-01-01 11:40:54 +0330

    Refactored code in services and repositories

--- src/main/java/com/aminnorouzi/ms/repository/UserRepository.java
+++ src/main/java/com/aminnorouzi/ms/repository/UserRepository.java
@@ -1,7 +1,9 @@
 package com.aminnorouzi.ms.repository;
 
 import com.aminnorouzi.ms.model.user.User;
+import org.jetbrains.annotations.NotNull;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
@@ -9,5 +11,12 @@ import java.util.Optional;
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
 
+    @Override
+    @NotNull
+    @Query("select u from User u left join fetch u.movies")
+    Optional<User> findById(@NotNull Long id);
+
     Optional<User> findByUsername(String username);
+
+    boolean existsByUsername(String username);
 }
