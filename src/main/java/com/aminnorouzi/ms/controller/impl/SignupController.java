50d2e6b58dbad928f6f5b54b8f9023427fdbee81
aminnorouzi <aminnorouzidev@gmail.com>
2023-01-12 00:01:34 +0330

    Added validation to inputs

--- src/main/java/com/aminnorouzi/ms/controller/impl/SignupController.java
+++ src/main/java/com/aminnorouzi/ms/controller/impl/SignupController.java
@@ -11,6 +11,7 @@ import com.aminnorouzi.ms.tool.view.View;
 import com.aminnorouzi.ms.tool.view.ViewSwitcher;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
 import javafx.scene.input.MouseEvent;
@@ -27,6 +28,8 @@ public class SignupController extends Controller {
     private TextField fullNameField;
     @FXML
     private PasswordField passwordField;
+    @FXML
+    private Button signupButton;
 
     public SignupController(ApplicationContext context, ViewSwitcher switcher, NotificationService notification,
                             LibraryService library, ActivityService activity) {
@@ -36,7 +39,13 @@ public class SignupController extends Controller {
 
     @Override
     protected void configure() {
-
+        signupButton.disableProperty().bind(fullNameField.textProperty().isEmpty()
+                .or(usernameField.textProperty().isEmpty())
+                .or(passwordField.textProperty().isEmpty())
+                .or(usernameField.textProperty().length().lessThan(5))
+                .or(usernameField.textProperty().length().greaterThan(16))
+                .or(passwordField.textProperty().length().lessThan(4))
+                .or(passwordField.textProperty().length().greaterThan(32)));
     }
 
     @FXML
