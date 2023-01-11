5e755ed3401ca570acf2f62bd295aa39178531a7
aminnorouzi <aminnorouzidev@gmail.com>
2023-01-12 00:06:39 +0330

    Implemented a task for loading movies

--- src/main/java/com/aminnorouzi/ms/controller/impl/LibraryController.java
+++ src/main/java/com/aminnorouzi/ms/controller/impl/LibraryController.java
@@ -22,15 +22,22 @@ import javafx.scene.input.MouseButton;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.TilePane;
 import javafx.scene.paint.Color;
+import javafx.scene.paint.ImagePattern;
+import javafx.scene.paint.Paint;
 import javafx.scene.shape.Rectangle;
 import javafx.scene.shape.StrokeLineCap;
 import javafx.scene.shape.StrokeLineJoin;
 import javafx.scene.shape.StrokeType;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
 import net.rgielen.fxweaver.core.FxmlView;
+import org.apache.commons.collections.map.AbstractOrderedMapDecorator;
+import org.controlsfx.control.GridCell;
+import org.controlsfx.control.GridView;
 import org.springframework.stereotype.Component;
 
+import java.util.Comparator;
 import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
@@ -39,8 +46,6 @@ import java.util.Map;
 @Component
 @FxmlView("/view/library-view.fxml")
 public class LibraryController extends Controller {
-
-//    private final LibraryTaskService task = new LibraryTaskService();
 
     private final Map<Rectangle, Movie> contents = new LinkedHashMap<>();
 
@@ -91,45 +96,23 @@ public class LibraryController extends Controller {
 
     @Override
     protected void configure() {
-        contents.clear();
-
         User user = getUser();
-        List<Movie> movies = user.getMovies();
-        movies.forEach(movie -> {
-            int index = movies.indexOf(movie) + 1;
-            System.out.println("#" + index + " " + movie.getTitle());
-        });
 
-        System.out.println();
+        List<Movie> movies = user.getMovies().stream()
+                .sorted(Comparator.comparing(Movie::getCreatedAt)
+                        .reversed())
+                .toList();
 
         movies.forEach(movie -> {
-//            addToScene(movie, false);
+            Rectangle rectangle = GraphicsManager.getARectangle();
+
+            body.getChildren().add(rectangle);
+            contents.put(rectangle, movie);
+
             LibraryTaskService task = new LibraryTaskService();
+            task.setRectangle(rectangle);
             task.setMovie(movie);
             task.restart();
-        });
-    }
-
-    public void addToScene(Movie movie, boolean index) {
-        Image image = new Image(movie.getPoster(), 200, 300, false, false, true);
-        image.progressProperty().addListener((observable, oldValue, progress) -> {
-            if ((Double) progress == 1.0 && !image.isError()) {
-                Rectangle rec = GraphicsManager.roundImage(image);
-                rec.setCursor(Cursor.HAND);
-
-                rec.setOnMouseEntered(mouseEnterEventHandler);
-                rec.setOnMouseExited(mouseExitEventHandler);
-                rec.setOnMouseClicked(mouseClickEventHandler);
-
-                contents.put(rec, movie);
-
-                if (index) {
-                    body.getChildren().add(0, rec);
-                    return;
-                }
-
-                body.getChildren().add(rec);
-            }
         });
     }
 
@@ -138,34 +121,36 @@ public class LibraryController extends Controller {
         switchTo(View.ADDITION);
     }
 
-    @Getter
     @Setter
-    public class LibraryTaskService extends Service<Rectangle> {
+    @Getter
+    public class LibraryTaskService extends Service<Image> {
 
         private Movie movie;
+        private Rectangle rectangle;
 
         @Override
-        protected Task<Rectangle> createTask() {
+        protected Task<Image> createTask() {
             return new Task<>() {
 
                 @Override
-                protected Rectangle call() throws Exception {
-//                    Image image = new Image(movie.getPoster(), 200, 300, false, false, false);
-//                    return GraphicsManager.roundImage(image);
-                    return new Rectangle();
+                protected Image call() throws Exception {
+                    return new Image(movie.getPoster(), 200, 300, false, false, false);
                 }
 
                 @Override
                 protected void succeeded() {
-                    Rectangle rec = getValue();
-                    rec.setCursor(Cursor.HAND);
-                    rec.setOnMouseEntered(mouseEnterEventHandler);
-                    rec.setOnMouseExited(mouseExitEventHandler);
-                    rec.setOnMouseClicked(mouseClickEventHandler);
+                    ImagePattern image = new ImagePattern(getValue());
 
-                    contents.put(rec, movie);
-                    body.getChildren().add(rec);
-                    System.out.println(movie.getTitle() + " added to scene.");
+                    rectangle.setFill(image);
+                    rectangle.setCursor(Cursor.HAND);
+                    rectangle.setOnMouseEntered(mouseEnterEventHandler);
+                    rectangle.setOnMouseExited(mouseExitEventHandler);
+                    rectangle.setOnMouseClicked(mouseClickEventHandler);
+                }
+
+                @Override
+                protected void failed() {
+                    // loads no poster movie image
                 }
             };
         }
