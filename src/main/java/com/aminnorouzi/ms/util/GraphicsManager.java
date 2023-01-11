477cc89f35f9916e8da7659b0cc1352cf81e8bf9
aminnorouzi <aminnorouzidev@gmail.com>
2023-01-12 00:02:28 +0330

    Added getARectangle method

--- src/main/java/com/aminnorouzi/ms/util/GraphicsManager.java
+++ src/main/java/com/aminnorouzi/ms/util/GraphicsManager.java
@@ -1,6 +1,7 @@
 package com.aminnorouzi.ms.util;
 
 import javafx.scene.image.Image;
+import javafx.scene.paint.Color;
 import javafx.scene.paint.ImagePattern;
 import javafx.scene.shape.Rectangle;
 import org.springframework.stereotype.Component;
@@ -16,6 +17,15 @@ public class GraphicsManager {
         ImagePattern pattern = new ImagePattern(image);
 
         rectangle.setFill(pattern);
+
+        return rectangle;
+    }
+
+    public static Rectangle getARectangle() {
+        Rectangle rectangle = new Rectangle(200, 300);
+        rectangle.setFill(Color.WHITE);
+        rectangle.setArcHeight(30);
+        rectangle.setArcWidth(30);
 
         return rectangle;
     }
