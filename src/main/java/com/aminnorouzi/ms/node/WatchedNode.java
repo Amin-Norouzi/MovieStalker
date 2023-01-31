bac8cb7ad42a9ee74243659352469f367c71e4cd
aminnorouzi <aminnorouzidev@gmail.com>
2023-01-31 18:17:39 +0330

    Fixed minor bugs

--- src/main/java/com/aminnorouzi/ms/node/WatchedNode.java
+++ src/main/java/com/aminnorouzi/ms/node/WatchedNode.java
@@ -1,30 +1,39 @@
 package com.aminnorouzi.ms.node;
 
+import com.aminnorouzi.ms.controller.Controller;
 import com.aminnorouzi.ms.exception.IllegalViewException;
 import com.aminnorouzi.ms.model.movie.Movie;
+import com.aminnorouzi.ms.tool.image.ImageService;
+import com.aminnorouzi.ms.tool.view.View;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.control.Label;
-import javafx.scene.image.Image;
+import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.HBox;
 import javafx.scene.paint.ImagePattern;
 import javafx.scene.shape.Rectangle;
+import lombok.RequiredArgsConstructor;
 
+@RequiredArgsConstructor
 public class WatchedNode extends HBox {
 
     private static final String PATH = "/templates/node/watched-node.fxml";
+    private final Controller controller;
+    private final Movie movie;
 
-    public WatchedNode(Movie movie, Integer count) {
+    public WatchedNode(Movie movie, Integer count, Controller controller) {
+        this.controller = controller;
+        this.movie = movie;
+
         try {
             FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource(PATH));
             fxmlLoader.setRoot(this);
             fxmlLoader.setController(this);
             fxmlLoader.load();
 
-            Image image = new Image(movie.getBackdrop(), true);
-            image.progressProperty().addListener((observable, oldValue, progress) -> {
-                if ((Double) progress == 1.0 && !image.isError()) {
-                    ((Rectangle) lookup("#poster")).setFill(new ImagePattern(image));
-                }
+            controller.getImage().load(movie.getBackdrop()).thenAccept(image -> {
+                ((Rectangle) lookup("#poster")).setFill(new ImagePattern(image));
             });
 
             ((Label) lookup("#rank")).setText(String.valueOf(count));
@@ -33,9 +42,13 @@ public class WatchedNode extends HBox {
             ((Label) lookup("#year")).setText(String.valueOf(movie.getReleased().getYear()));
             ((Label) lookup("#genre")).setText(movie.getGenres().get(0));
         } catch (Exception exception) {
-            System.out.println(exception.getMessage());
             throw new IllegalViewException(exception.getMessage());
         }
+    }
+
+    @FXML
+    private void onMovie(MouseEvent event) {
+        controller.switchTo(View.MOVIE, movie);
     }
 }
 
