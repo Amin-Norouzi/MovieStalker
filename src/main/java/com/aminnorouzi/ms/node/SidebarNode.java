33c969d7e6382df6c807ec44a54c7801f2dc791d
aminnorouzi <aminnorouzidev@gmail.com>
2023-02-14 01:58:55 +0330

    Updated the design and fixed bugs

--- src/main/java/com/aminnorouzi/ms/node/SidebarNode.java
+++ src/main/java/com/aminnorouzi/ms/node/SidebarNode.java
@@ -6,14 +6,24 @@ import com.aminnorouzi.ms.tool.view.View;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
-import javafx.scene.Node;
+import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
-import javafx.scene.layout.VBox;
+import javafx.scene.layout.HBox;
 
-public class SidebarNode extends VBox {
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class SidebarNode extends HBox implements Initializable {
 
     private static final String PATH = "/templates/node/sidebar-node.fxml";
     private final Controller controller;
+
+    @FXML
+    private Button discoverButton;
+    @FXML
+    private Button homeButton;
+    @FXML
+    private Button libraryButton;
 
     public SidebarNode(Controller controller) {
         this.controller = controller;
@@ -24,26 +34,31 @@ public class SidebarNode extends VBox {
             fxmlLoader.setController(this);
             fxmlLoader.load();
 
-            // Workaround to change the button state pressed
-            String title = View.getTitle(controller.getView());
-            for (Node node : ((VBox) lookup("#container")).getChildren()) {
-                if (node instanceof Button) {
-                    String text = ((Button) node).getText().toLowerCase();
-                    if (text.equals(title)) {
-                        node.setId(String.format("%s-button-clicked", title));
-                        break;
-                    }
-                }
-            }
         } catch (Exception exception) {
-            System.out.println(exception.getMessage());
             throw new IllegalViewException(exception.getMessage());
+        }
+    }
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        // Workaround to change the button state pressed
+        String id = "%s-button-clicked";
+        String title = View.getTitle(controller.getView());
+        switch (title.concat("Button")) {
+            case "homeButton" -> homeButton.setId(String.format(id, title));
+            case "discoverButton" -> discoverButton.setId(String.format(id, title));
+            case "libraryButton" -> libraryButton.setId(String.format(id, title));
         }
     }
 
     @FXML
     private void onHome(ActionEvent event) {
         controller.switchTo(View.HOME);
+    }
+
+    @FXML
+    private void onDiscover(ActionEvent event) {
+        controller.switchTo(View.DISCOVER);
     }
 
     @FXML
