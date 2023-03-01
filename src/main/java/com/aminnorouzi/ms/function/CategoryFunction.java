04c5eac58d9c2beb01c664c2cb7ad86e34d3f4b5
aminnorouzi <aminnorouzidev@gmail.com>
2023-03-01 22:45:37 +0330

    Renamed genre to category

--- src/main/java/com/aminnorouzi/ms/function/CategoryFunction.java
+++ src/main/java/com/aminnorouzi/ms/function/CategoryFunction.java
@@ -1,15 +1,15 @@
 package com.aminnorouzi.ms.function;
 
 import com.aminnorouzi.ms.controller.Controller;
-import com.aminnorouzi.ms.node.GenreNode;
+import com.aminnorouzi.ms.node.CategoryNode;
 import javafx.scene.Node;
 
 import java.util.function.BiFunction;
 
-public class GenreFunction implements BiFunction<Controller, Object, Node> {
+public class CategoryFunction implements BiFunction<Controller, Object, Node> {
 
     @Override
     public Node apply(Controller controller, Object value) {
-        return new GenreNode(controller, (String) value);
+        return new CategoryNode(controller, (String) value);
     }
 }
